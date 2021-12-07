e41f78b26d4d9e345408094293bd2a3b30cb667a
kostalexis89 <kostalexis.hosting@gmail.com>
2021-12-07 18:12:23 +0100

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './components/Home';
+import  Header  from './components/Header';
+import Beers from "./components/Beers"
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/beers" element={<Beers />} />
       </Routes>
     </div>
   );
